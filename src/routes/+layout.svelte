3ed313a83998bff51607be946179ac0a69467e75
StudioMystery <team@studiomystery.com>
2024-12-19 18:19:44 -0600

    fix: moved GTM tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,18 +6,6 @@
   import Footer from "$lib/components/Footer.svelte";
 </script>
 
-<!-- Google tag (gtag.js) -->
-<Head>
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-T6RJY7HY09"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-T6RJY7HY09');
-  </script>
-</Head>
-
 <Container>
   <Header />
   <Content>
